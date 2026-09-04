afb97250b6df09c681ac9689a25c5e8911563857
Umair Siddique <hafizumairsiddiqie786@gmail.com>
2026-09-04 07:42:40 +0500

    security: register people plan limit service

--- backend/src/modules/people/people.module.ts
+++ backend/src/modules/people/people.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PeopleService } from './people.service';
 import { PeopleController } from './people.controller';
+import { PlanLimitService } from './plan-limit.service';
 
 @Module({
   controllers: [PeopleController],
-  providers: [PeopleService],
-  exports: [PeopleService],
+  providers: [PeopleService, PlanLimitService],
+  exports: [PeopleService, PlanLimitService],
 })
 export class PeopleModule {}
