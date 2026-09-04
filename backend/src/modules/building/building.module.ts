f3ecb2304d26eea775766b4ca54ecc35b617786c
Umair Siddique <hafizumairsiddiqie786@gmail.com>
2026-09-04 11:05:24 +0500

    fix: provide plan limits to building module

--- backend/src/modules/building/building.module.ts
+++ backend/src/modules/building/building.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BuildingController } from './building.controller';
 import { BuildingService } from './building.service';
 import { DatabaseModule } from '../database/database.module';
+import { PeopleModule } from '../people/people.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, PeopleModule],
   controllers: [BuildingController],
   providers: [BuildingService],
   exports: [BuildingService],
