2e083a326a17bd18581436ec2ebd290daeaec367
Umair Siddique <hafizumairsiddiqie786@gmail.com>
2026-09-04 16:02:00 +0500

    feat(students): connect credentials dialog to app shell

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './theme-overrides.css'
 import App from './App.tsx'
+import StudentCredentialsBridge from './components/students/StudentCredentialsBridge'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <StudentCredentialsBridge />
   </StrictMode>,
 )
