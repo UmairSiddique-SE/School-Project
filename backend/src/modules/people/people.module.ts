fe28e170a81617482ae282f8d615ccfb1c03327d
Umair Siddique <hafizumairsiddiqie786@gmail.com>
2026-09-15 20:13:36 +0500

    feat: register bulk student controller

--- backend/src/modules/people/people.module.ts
+++ backend/src/modules/people/people.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PeopleService } from './people.service';
 import { PeopleController } from './people.controller';
+import { StudentBulkController } from './student-bulk.controller';
 import { PlanLimitService } from './plan-limit.service';
 import './people.service.compat';
 
 @Module({
-  controllers: [PeopleController],
+  controllers: [PeopleController, StudentBulkController],
   providers: [PeopleService, PlanLimitService],
   exports: [PeopleService, PlanLimitService],
 })
