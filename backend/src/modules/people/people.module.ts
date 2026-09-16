40735e5a98b24d48a0519a7b46fc70ec1be93393
Umair Siddique <2025se364@student.uet.edu.pk>
2026-09-16 22:30:13 +0500

    feat: register student profile persistence controller

--- backend/src/modules/people/people.module.ts
+++ backend/src/modules/people/people.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { PeopleService } from './people.service';
 import { PeopleController } from './people.controller';
 import { StudentBulkController } from './student-bulk.controller';
+import { StudentRecordController } from './student-record.controller';
 import { PlanLimitService } from './plan-limit.service';
 import './people.service.compat';
 
 @Module({
-  controllers: [PeopleController, StudentBulkController],
+  controllers: [PeopleController, StudentBulkController, StudentRecordController],
   providers: [PeopleService, PlanLimitService],
   exports: [PeopleService, PlanLimitService],
 })
